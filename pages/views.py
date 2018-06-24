498bda5e3c4f353adaea48cec1ca387c3a7c0e08
yurabysaha <yurabysaha@gmail.com>
2018-06-24 21:44:02 +0300

    Add feedback form and logic Fix. change model name Client_Company to ClientCompany

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,23 @@
+from django.contrib import messages
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
-# Create your views here.
+from pages.forms import FeedbackForm
+from pages.models import Feedback
+
+
+def feedback(request):
+    """Create or show list for feedback"""
+    if request.method == "POST":
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Ваш відгук буде опубліковано після перевірки модератором. Дякуємо!')
+
+    form = FeedbackForm()
+
+    feedbacks = Paginator(Feedback.objects.exclude(is_published=False), 10)
+    page = request.GET.get('page')
+    feedbacks = feedbacks.get_page(page)
+
+    return render(request, 'pages/feedback.html', {'feedbacks': feedbacks, 'form': form})
