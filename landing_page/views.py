7f02eb9f7c0803ff1264f377a71c1260dc606993
Your Name <sanya.seredyak@gmail.com>
2018-07-03 13:56:38 +0300

    Added fixtures for model "Category" Change category block (added include for this bloc to all pages) Refactoring Journey cards and added Journey card for sale price

--- landing_page/views.py
+++ landing_page/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from product.models import Journey
+from product.models import Journey, Category
 from pages.models import Feedback
 from news.models import News
 
@@ -8,6 +8,8 @@ def home(request):
     journeys = Journey.objects.all()[:8]
     feedbacks = Feedback.objects.all()[:5]
     newses = News.objects.last()
+    categories = Category.objects.all()
     return render(request, 'landing_page/home.html', {'journeys': journeys,
                                                       'feedbacks': feedbacks,
-                                                      'newses': newses})
+                                                      'newses': newses,
+                                                      'categories': categories})
--- product/views.py
+++ product/views.py
@@ -7,7 +7,7 @@ def category_detail(request, slug):
     category = get_object_or_404(Category, slug=slug)
     categories = Category.objects.all()
     journeys = Journey.objects.filter(category=category)
-    journeys_with_sale = Journey.objects.filter(category=category).exclude(sale_price__isnull=True)
+    journeys_with_sale = Journey.objects.filter(category=category).exclude(sale_price__isnull=True)[:5]
     return render(request, 'product/Category_detail.html', {'category': category,
                                                             'categories': categories,
                                                             'journeys': handle_pagination(request, journeys),
