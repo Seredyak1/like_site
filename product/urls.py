daafab4338519b07b47e7a02bc2785a3efd45e02
grigorevskiy <grigorevskiiy@gmail.com>
2018-07-12 14:40:17 +0300

    Added methods for comments

---
+++ product/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from product import views
+
+urlpatterns = [
+    path('<int:journey_id>/', views.journey_details, name='journey_details'),
+    path('<int:journey_id>/comment/<int:comment_id>/delete/', views.comment_delete, name='comment_delete'),
+    path('<int:journey_id>/comment/create/', views.create_comment, name='create_comment'),
+    path('<int:journey_id>/comment/<int:comment_id>/update', views.update_comment, name='update_comment')
+ ]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import CommentForm, Comment
+from .models import Journey
+from django.http import Http404
 
-# Create your views here.
+
+def journey_details(request, journey_id):
+    journey = Journey.objects.filter(id=journey_id).first()
+    if not journey:
+        raise Http404
+    comments = Comment.objects.filter(journey=journey)
+    form = CommentForm()
+
+    return render(request, 'product/item_details.html', {'journey': journey, 'comments': comments, 'form': form})
+
+
+def create_comment(request, journey_id):
+    if request.user.is_authenticated:
+        journey = get_object_or_404(Journey, id=journey_id)
+        if request.method == "POST":
+            comment = Comment(body=request.POST['body'], journey=journey, user=request.user)
+            form = CommentForm(request.POST, instance=comment)
+            if form.is_valid():
+                form.save()
+                return redirect('/journey/{}'.format(journey_id))
+    else:
+        return redirect('/')
+
+
+def update_comment(request, comment_id, journey_id):
+    if request.user.is_authenticated:
+        comment = get_object_or_404(Comment, id=comment_id, user=request.user.id)
+        if request.method == "POST":
+            form = CommentForm(request.POST, instance=comment)
+            if form.is_valid():
+                form.save()
+                return redirect('/journey/{}'.format(journey_id))
+        else:
+            form = CommentForm(instance=comment)
+        return render(request, 'product/item_details.html', {'form': form})
+    else:
+        return redirect('/')
+
+
+def comment_delete(request, comment_id, journey_id):
+    if request.user.is_authenticated:
+        comment = get_object_or_404(Comment, id=comment_id, user=request.user.id)
+        if request.method == "POST":
+            comment.delete()
+            return redirect('/journey/{}'.format(journey_id))
+        return render(request, "product/item_details.html")
+    else:
+        return redirect('/')
