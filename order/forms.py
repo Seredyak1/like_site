46c54914f15fb54dbe26f3668d7f998344ab4865
Your Name <sanya.seredyak@gmail.com>
2018-07-18 16:13:19 +0300

    Added form to order_anonim, setting admin to show orders

---
+++ order/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from order.models import Order, OrderAnonim
+
+
+class CreateOrderAnonim(forms.ModelForm):
+
+    name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), label="Призвіще та ім'я")
+    duration = forms.IntegerField(label="Тривалість")
+    person = forms.IntegerField(label="Кількість осіб")
+    description = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 4}), label='Що б Ви хотіли побачити?')
+    phone = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}), label="Номер телефону")
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control'}), label="Email-адреса")
+
+    class Meta:
+        model = OrderAnonim
+        fields = ('name', 'person', 'duration', 'description', 'phone', 'email')
