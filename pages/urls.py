b9882850af9718fcc4783ca9b42a09dfa81cd4a0
Your Name <sanya.seredyak@gmail.com>
2018-07-22 23:43:27 +0300

    Added Search page, config logic for search

--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('feedback', views.feedback, name="feedback"),
     path('about_us', views.about_us, name="about_us"),
     path('faq', views.get_faq, name="faq"),
+    path('search', views.search, name="search"),
 ]
