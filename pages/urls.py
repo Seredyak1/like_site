bad161a48c4885dfe228278878d775d2e8f76e6c
Your Name <sanya.seredyak@gmail.com>
2019-07-03 20:07:49 +0300

    Fix name issue

--- pages/urls.py
+++ pages/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('about_us/', views.about_us, name="about_us"),
     path('faq/', views.FAQView.as_view(), name="faq"),
     path('search/', views.search, name="search"),
-    path('documents/', views.DocumentsView.as_view(), name="documents"),
+    path('documents/', views.documents, name="documents"),
     path('documents/download/<int:file_id>/', views.download_file, name="download_file"),
 ]
